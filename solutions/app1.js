0cee57a8ea5519e062384bc1df704b703fb298bd
Hajni83 <maro.hajnalka@gmail.com>
2021-11-17 11:39:05 +0100

    1 task finished

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,6 +1,19 @@
+const converterPromise = (array) => {
+    return new Promise ((resolve, reject) => {
+        let arr=array.map(x => {
+            if (typeof x !== 'string') {
+                reject('Error: Not all elements are string type!');
+            }
+            return x.toString().toUpperCase();
+        });
+        if (arr) {
+            resolve(arr);
+        }
+    })
+};
 
 
-
+export default converterPromise;
 
 
 
